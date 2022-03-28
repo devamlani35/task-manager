7e84c9ad4dc94126b878cb9ed81f7da9281025de
Dev Amlani <amlanid35@gmail.com>
2022-03-28 17:57:35 -0400

    Terminate process file

---
+++ src/format-data/kill_process.py
@@ -0,0 +1,16 @@
+import sys
+from psutil import Process
+import os
+if __name__ == "__main__":
+  pid = int(sys.argv[1])
+  if pid == os.getpid():
+    print(0)
+  else:
+    p = Process(pid)
+    kill_arr = []
+    kill_arr+= p.children(recursive=True)
+    kill_arr.append(p)
+    for process in kill_arr:
+      process.terminate()
+    print(1)
+  
